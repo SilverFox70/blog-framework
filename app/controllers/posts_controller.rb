5efc87bc8a108beb8436dbc1b6ab118e7068cf59
SilverFox70 <wfbrinkert@gmail.com>
2016-03-31 19:21:33 -0700

    Add more logic for viewing previous and later posts to both posts controllor and posts index erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,18 @@ class PostsController < InheritedResources::Base
 		puts "-" * 30
 		puts params
 		puts "-" * 30
-		start = params[:start] if params{:start} != nil
-		@posts = Post.all
+		# if no start params given, then return the 3 most
+		# recent posts; otherwise, get the most recent posts
+		# offset by :start e.g. start = -3, get the fourth
+		# fifth, and sixth most recent posts. :start must 
+		# track through each call.
+		if params[:start].nil?
+			@start = 1
+			@posts = Post.order("id asc").limit(3)
+		else @start = params[:start].to_i 
+			stop = @start + 3
+			@posts = Post.where(:id => @start..stop).order("id asc")
+		end
 	end
 
   private
