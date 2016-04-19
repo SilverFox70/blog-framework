154531d8f28e243b75401b0ca93be8fa6972465c
SilverFox70 <wfbrinkert@gmail.com>
2016-04-19 13:19:06 -0700

    Clean up comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,27 +8,21 @@ class CommentsController < InheritedResources::Base
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(comment_params)
-    puts "%" * 30
-    puts "#{@comment.to_json}"
-    puts "%" * 30
-    # respond_to do |format|
-    #   format.json {render json:@comment}
-    # end
-    if !request.xhr?
-  		redirect_to post_path(@post)
-    else
-      render partial: "comment", locals: { comment: @comment }
-    end
+	    if !request.xhr?
+	  		render post_path(@post)
+	    else
+	      render partial: "comment", locals: { comment: @comment }
+	    end
 	end
 
 	def edit
 		c = Comment.find_by_id(params[:id])
 		!c.nil? ? comment_author = c.user_id : comment_author = nil
 		if current_user && current_user.id == comment_author
-			@comment = Comment.find(params[:id])
-      render "edit"
+		  @comment = Comment.find(params[:id])
+	      render "edit"
 		else
-			redirect_to "/users/sign_in"
+		  redirect_to "/users/sign_in"
 		end
 	end
 
